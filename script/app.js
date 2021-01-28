b1518d3e024d4074d6ce848c8205a73d445ef7cd
JoelSilvi <analistasilvi@gmail.com>
2021-01-28 16:33:56 -0300

    arreglando index

--- script/app.js
+++
@@ -1,7 +0,0 @@
-// const d = document;
-
-// export default function contactFormValidations(){
-//     const $form = d.querySelector(".form-horizontal"),
-//     $inputs = d.querySelectorAll(".form-horizontal [required]");
-//     console.log($inputs);
-// }
